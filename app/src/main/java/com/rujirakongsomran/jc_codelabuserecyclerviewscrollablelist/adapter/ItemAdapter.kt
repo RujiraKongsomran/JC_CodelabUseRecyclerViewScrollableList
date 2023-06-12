c1fa7b7c0813271649c2c5deb0b92ee21f5335b4
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-06-12 15:39:52 +0700

    Create an ItemAdapter class

---
+++ app/src/main/java/com/rujirakongsomran/jc_codelabuserecyclerviewscrollablelist/adapter/ItemAdapter.kt
@@ -0,0 +1,10 @@
+package com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist.adapter
+
+import android.content.Context
+import com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist.model.Affirmation
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) {
+}
