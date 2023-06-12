7aafe3fd8fafd6f67c8c3468a672f39ba637ae1a
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-06-12 15:43:26 +0700

    Create a ViewHolder

--- app/src/main/java/com/rujirakongsomran/jc_codelabuserecyclerviewscrollablelist/adapter/ItemAdapter.kt
+++ app/src/main/java/com/rujirakongsomran/jc_codelabuserecyclerviewscrollablelist/adapter/ItemAdapter.kt
@@ -1,10 +1,17 @@
 package com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist.adapter
 
 import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist.R
 import com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist.model.Affirmation
 
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Affirmation>
 ) {
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textview: TextView = view.findViewById(R.id.item_title)
+    }
 }
