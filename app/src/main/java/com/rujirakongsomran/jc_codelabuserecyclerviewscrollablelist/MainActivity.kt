3c78067481bd8f77ba582ae6f6a7d3927148ef1e
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-06-12 17:52:21 +0700

    Adding images to the list items

--- app/src/main/java/com/rujirakongsomran/jc_codelabuserecyclerviewscrollablelist/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_codelabuserecyclerviewscrollablelist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist.adapter.ItemAdapter
+import com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist.data.Datasource
 import com.rujirakongsomran.jc_codelabuserecyclerviewscrollablelist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val myDataset = Datasource().loadAffirmations()
+        binding.recyclerView.adapter = ItemAdapter(this, myDataset)
+        binding.recyclerView.setHasFixedSize(true)
 
 
     }
